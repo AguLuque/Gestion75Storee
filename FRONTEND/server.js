abba5594fda78ea2c961ae2b10c93ad6a058a447
AguLuque <agustinluque.lbz@gmail.com>
2026-05-15 16:24:26 -0300

    fix: update express 5 wildcard route syntax

--- FRONTEND/server.js
+++ FRONTEND/server.js
@@ -7,8 +7,8 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.get('/{*path}', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
